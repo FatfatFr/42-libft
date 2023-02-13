2a1215549c8f351e829a091b69125c05084dcda4
fatoudiallo <fatous.diallo@yahoo.fr>
2023-02-13 21:32:11 +0100

    test

---
+++ ft_strnstr.c
@@ -0,0 +1,19 @@
+char *ft_strnstr(const char *haystack, const char *needle, size_t len) 
+{
+    size_t needle_len;
+
+    needle_len = strlen(needle);
+    if (needle_len == 0) 
+    {
+        return ((char *)haystack);
+    }
+    while (*haystack && len-- >= needle_len) 
+    {
+        if (*haystack == *needle && ft_strncmp(haystack, needle, needle_len) == 0)
+        {
+            return ((char *)haystack);
+        }
+        haystack++;
+    }
+    return (NULL);
+}
