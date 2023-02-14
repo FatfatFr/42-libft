fcfd8d761cfbecbc31b6cfcae12c4dbfac3fec81
Fatou Diallo <fatdiall@c1r1p2.42perpignan.fr>
2023-02-14 17:36:51 +0100

    all functions

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,8 +6,30 @@
 /*   By: fatdiall <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 17:27:42 by fatdiall          #+#    #+#             */
-/*   Updated: 2023/02/01 17:27:45 by fatdiall         ###   ########.fr       */
+/*   Updated: 2023/02/14 17:33:54 by fatdiall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_memcmp(const void *ptr1, const void *ptr2, size_t num) 
+{
+	const unsigned char *p1;
+	const unsigned char *p2;
 
+	p1 = (const unsigned char *)ptr1;
+ 	p2 = (const unsigned char *)ptr2;
+	while (num-- > 0) 
+	{
+		if (*p1++ != *p2++) 
+		{
+			return *(p1 - 1) - *(p2 - 1);
+		}
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char	one[] = "";
+	char	two[] = "";
+	return (0);
+}
