8b7e5ec892d0682fc6d3557fc4c4ffe76d52f31c
Fatou Diallo <fatdiall@c1r1p2.42perpignan.fr>
2023-03-09 10:27:28 +0100

    okay

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,38 +5,74 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fatdiall <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/01 18:12:15 by fatdiall          #+#    #+#             */
-/*   Updated: 2023/03/08 18:49:14 by fatdiall         ###   ########.fr       */
+/*   Created: 2023/03/09 10:24:30 by fatdiall          #+#    #+#             */
+/*   Updated: 2023/03/09 10:24:55 by fatdiall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	c;
+	char	buffer[11];
+	int		i;
 
-	c = (n % 10) + '0';
 	if (n == -2147483648)
 	{
 		write(fd, "-2147483648", 11);
 		return ;
 	}
 	if (n < 0)
-	{	n = -n;
+	{
+		n = -n;
 		write(fd, "-", 1);
 	}
-	if (n > 9)
+	i = 0;
+	while (n > 9)
 	{
-		ft_putnbr_fd(n / 10, fd);
+		buffer[i++] = (n % 10) + '0';
+		n /= 10;
 	}
-	write(fd, &c, 1);
+	buffer[i] = n + '0';
+	while (i >= 0)
+i		write(fd, &buffer[i--], 1);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	int	n = 17;
 	int	i;
 	i = 1;
 	ft_putnbr_fd(n, i);
 }
+
+void    ft_putnbr_fd(int n, int fd)
+{
+    char    digits[10];
+    int     sign = 1;
+    int     i = 0;
+
+    if (n == 0)
+    {
+        write(fd, "0", 1);
+        return ;
+    }
+    if (n < 0)
+    {
+        sign = -1;
+        n = -n;
+    }
+    while (n > 0)
+    {
+        digits[i++] = (n % 10) + '0';
+        n /= 10;
+    }
+    if (sign == -1)
+    {
+        write(fd, "-", 1);
+    }
+    while (--i >= 0)
+    {
+        write(fd, &digits[i], 1);
+    }
+}*/
