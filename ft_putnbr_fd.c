1e9432f8dc39f61ea6a0c5524d253bb6bb1754b8
Fatou Diallo <fatdiall@c1r1p2.42perpignan.fr>
2023-03-09 16:55:21 +0100

    clone

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: fatdiall <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 10:24:30 by fatdiall          #+#    #+#             */
-/*   Updated: 2023/03/09 10:24:55 by fatdiall         ###   ########.fr       */
+/*   Updated: 2023/03/09 15:29:56 by fatdiall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	buffer[11];
-	int		i;
+	char	c;
 
+	c = n % 10 + '0';
 	if (n == -2147483648)
 	{
 		write(fd, "-2147483648", 11);
@@ -24,55 +24,21 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (n < 0)
 	{
-		n = -n;
-		write(fd, "-", 1);
+		write(fd, "-", fd);
+		n *= -1;
 	}
-	i = 0;
-	while (n > 9)
+	if (n > 0)
 	{
-		buffer[i++] = (n % 10) + '0';
-		n /= 10;
+		ft_putnbr_fd(n / 10, fd);
+		write(fd, &c, fd);
 	}
-	buffer[i] = n + '0';
-	while (i >= 0)
-i		write(fd, &buffer[i--], 1);
 }
 
 /*int	main(void)
 {
-	int	n = 17;
+	int	n = 20;
 	int	i;
 	i = 1;
 	ft_putnbr_fd(n, i);
-}
-
-void    ft_putnbr_fd(int n, int fd)
-{
-    char    digits[10];
-    int     sign = 1;
-    int     i = 0;
-
-    if (n == 0)
-    {
-        write(fd, "0", 1);
-        return ;
-    }
-    if (n < 0)
-    {
-        sign = -1;
-        n = -n;
-    }
-    while (n > 0)
-    {
-        digits[i++] = (n % 10) + '0';
-        n /= 10;
-    }
-    if (sign == -1)
-    {
-        write(fd, "-", 1);
-    }
-    while (--i >= 0)
-    {
-        write(fd, &digits[i], 1);
-    }
+	return (0);
 }*/
